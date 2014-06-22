b698d1a7fe8447c392e1dbdccadbe6ca4d7611f6
Artur Balabanov <arturbalabanov97@gmail.com>
2014-06-22 19:53:00 +0300

    Add search

---
+++ app/js/search.js
@@ -0,0 +1,37 @@
+$(document).ready(function() {
+  $('.search-field').simpleJekyllSearch({
+    jsonFile : '/search.json',
+    searchResults : '.search-results',
+    template : '<li><article><a href="{url}">{title} <span class="entry-date"><time datetime="{date}">{shortdate}</time></span></a></article></li>',
+    noResults: '<p>Nothing found.</p>'
+  });
+});
+
+(function( $, window, undefined ) {
+  
+  var bs = {
+    close: $(".icon-remove-sign"),
+    searchform: $(".search-form"),
+    canvas: $("body"),
+    dothis: $('.dosearch')
+  };
+  
+  bs.dothis.on('click', function() {
+    $('.search-wrapper').css({ display: "block" });
+    bs.searchform.toggleClass('active');
+    bs.searchform.find('input').focus();
+    bs.canvas.toggleClass('search-overlay');
+  });
+  
+  // bs.close.on('click', function() {
+  //   $('.search-wrapper').removeAttr( 'style' );
+  //   bs.searchform.toggleClass('active');
+  //   bs.canvas.removeClass('search-overlay');
+  // });
+
+  bs.searchform.find('input').focusout(function () {
+    $('.search-wrapper').removeAttr( 'style' );
+    bs.searchform.toggleClass('active');
+    bs.canvas.removeClass('search-overlay');
+  });
+})( jQuery, window );
