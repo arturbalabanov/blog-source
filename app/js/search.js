f26e0caef0d75b20fad9d69ce7e6e00dd7ea83bc
Artur Balabanov <arturbalabanov97@gmail.com>
2014-06-22 20:09:18 +0300

    jshintify some files...

--- app/js/search.js
+++ app/js/search.js
@@ -1,5 +1,6 @@
-$(document).ready(function() {
-  $('.search-field').simpleJekyllSearch({
+/*global jQuery */
+jQuery(document).ready(function() {
+  jQuery('.search-field').simpleJekyllSearch({
     jsonFile : '/search.json',
     searchResults : '.search-results',
     template : '<li><article><a href="{url}">{title} <span class="entry-date"><time datetime="{date}">{shortdate}</time></span></a></article></li>',
@@ -10,14 +11,14 @@ $(document).ready(function() {
 (function( $, window, undefined ) {
   
   var bs = {
-    close: $(".icon-remove-sign"),
-    searchform: $(".search-form"),
-    canvas: $("body"),
+    close: $('.icon-remove-sign'),
+    searchform: $('.search-form'),
+    canvas: $('body'),
     dothis: $('.dosearch')
   };
   
   bs.dothis.on('click', function() {
-    $('.search-wrapper').css({ display: "block" });
+    $('.search-wrapper').css({ display: 'block' });
     bs.searchform.toggleClass('active');
     bs.searchform.find('input').focus();
     bs.canvas.toggleClass('search-overlay');
