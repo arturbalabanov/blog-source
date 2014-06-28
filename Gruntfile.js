f1f7f5042e68a1bdbb0ace1fb5d40a287ca5e3bf
Artur Balabanov <arturbalabanov97@gmail.com>
2014-06-28 20:38:45 +0300

    Small change in Gruntfile

--- Gruntfile.js
+++ Gruntfile.js
@@ -282,7 +282,7 @@ module.exports = function (grunt) {
             '!**/_*{,/**}',
             // Explicitly add any files your site needs for distribution here.
             '_bower_components/jquery/jquery.js',
-            '_bower_components/fontawesome'
+            '_bower_components/fontawesome/'
             //'favicon.ico',
             //'apple-touch*.png'
           ],
