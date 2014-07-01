a2343ee4158832e057f8ccb123f349867b0b2b9a
Artur Balabanov <arturbalabanov97@gmail.com>
2014-07-01 10:47:17 +0300

    Going back to previous variant

--- Gruntfile.js
+++ Gruntfile.js
@@ -281,8 +281,7 @@ module.exports = function (grunt) {
             // Like Jekyll, exclude files & folders prefixed with an underscore.
             '!**/_*{,/**}',
             // Explicitly add any files your site needs for distribution here.
-            '_bower_components/jquery/jquery.js',
-            '_bower_components/fontawesome/**/*'
+            // '_bower_components/jquery/jquery.js',
             //'favicon.ico',
             //'apple-touch*.png'
           ],
