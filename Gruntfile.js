e83d8bbdc75d1dd69f07769ed71432d0cb36c4cc
Artur Balabanov <arturbalabanov97@gmail.com>
2014-07-02 12:47:56 +0300

    Add only the fonts in the Gruntfile

--- Gruntfile.js
+++ Gruntfile.js
@@ -282,6 +282,7 @@ module.exports = function (grunt) {
             '!**/_*{,/**}',
             // Explicitly add any files your site needs for distribution here.
             // '_bower_components/jquery/jquery.js',
+            '_bower_components/fontawesome/fonts/*',
             //'favicon.ico',
             //'apple-touch*.png'
           ],
